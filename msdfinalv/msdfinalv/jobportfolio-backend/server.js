988e7efc20b4581754d4a18a719991d97bf38301
yasaswi-popuri <yasaswi2206@gmail.com>
2025-11-03 22:37:57 +0530

    Update CORS middleware to specify origin and credentials

--- msdfinalv/msdfinalv/jobportfolio-backend/server.js
+++ msdfinalv/msdfinalv/jobportfolio-backend/server.js
@@ -12,7 +12,10 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://job-portfolio-gules.vercel.app/',
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
